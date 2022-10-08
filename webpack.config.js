441f1e8478b239a706b9218c0da496ec935714ba
donggov <donggov@gmail.com>
2022-10-08 21:48:42 +0900

    speech bubble

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = function (env, argv) {
       "01_navmesh/js/main": "./01_navmesh/js/main.js",
       "02_tiled_and_player/js/main": "./02_tiled_and_player/js/main.js",
       "03_fog_of_field/js/main": "./03_fog_of_field/js/main.js",
+      "04_speech_bubble/js/main": "./04_speech_bubble/js/main.js",
     },
     output: {
       filename: "[name].js",
